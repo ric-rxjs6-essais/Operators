a287f94c7daf42103f59f8a58ebb7ecf441e93bf
Richard PYT <rick.info.rsoc@gmail.com>
2020-06-08 16:49:07 +0200

    First commit - prêt pour essais d'Operators RxJs 6

---
+++ _src/app/_main.ts
@@ -0,0 +1,11 @@
+// import { Observable, interval } from "rxjs";
+                                            
+export class Main {
+    public run(): void {
+        // const oObservable: Observable<number> = interval(1000);
+        // oObservable.subscribe( (piCounter: number) => {
+        //     console.log(`${piCounter}`);
+        // });
+    }        
+
+}
